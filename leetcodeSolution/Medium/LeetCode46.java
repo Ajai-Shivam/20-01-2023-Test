39bfcb620c46f21cbfe3555f7b3f96c7d60f6546
Ajai-Shivam <121566845+Ajai-Shivam@users.noreply.github.com>
2023-03-04 10:28:17 +0530

    Create LeetCode46.java

---
+++ leetcodeSolution/Medium/LeetCode46.java
@@ -0,0 +1,31 @@
+class LeetCode46 {
+    public List<List<Integer>> permute(int[] arr) {
+     List<List<Integer>> result  = new ArrayList<>();
+        backtrack(result,arr,0);
+        return result;
+    }
+    public void backtrack(List<List<Integer>> result,int[] arr , int start){
+        if(start == arr.length){
+            result.add(tolist(arr));
+        }
+        else{
+            for (int i = start; i < arr.length; i++) {
+                swap (i,start,arr);
+                backtrack(result,arr,start+1);
+                swap(i,start,arr);
+            }
+        }
+    }
+    public void swap(int i ,int j , int[] arr){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public List<Integer> tolist(int[] arr){
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            list.add(arr[i]);
+        }
+        return list;
+    }
+}
